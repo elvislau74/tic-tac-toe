ca4b0092601ce49a1d25d2ad2fd1f2433eb95dc2
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-08-22 21:19:54 -0400

    add code to typeDefs and resolvers files in schemas folder in server, edit expiration date in auth file in server folder, add auth file in client folder

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,22 @@
+// const gql = require('graphql-tag');
+
 const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    me: User
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+}
 `;
 
 module.exports = typeDefs;
