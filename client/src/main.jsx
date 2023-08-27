66b50db437851e31799a98c2d8914315933a0ae7
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-08-27 09:09:42 -0400

    start editing app and main files, add signupform component and fix loginform component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/game',
+        element: <Game />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      }
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
