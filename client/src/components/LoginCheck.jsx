3e38e9b4e0f601c7c246b60db81c2112cc1017ed
elvislau74 <126195092+elvislau74@users.noreply.github.com>
2023-08-29 15:52:53 -0400

    add logincheck component

---
+++ client/src/components/LoginCheck.jsx
@@ -0,0 +1,44 @@
+import { useQuery } from "@apollo/client";
+import { useEffect } from "react";
+import { useLogin } from "../utils/LoginContext";
+import { QUERY_ME } from "../utils/queries"
+import { LOGIN, LOGOUT } from "../utils/actions";
+import Auth from "../utils/auth";
+
+export default function LoginCheck(props) {
+    const [state, dispatch] = useLogin();
+    const { loading, data: loginData } = useQuery(QUERY_ME);
+
+    const token = state.token.length > 0 ? state.token : (Auth.getToken() || '');
+
+    const loggedIn = token.length > 0;
+
+    const user = loginData?.me || {};
+
+    useEffect(() => {
+        if (!loading) {
+            if (loggedIn && dispatch) {
+                Auth.login(token);
+                dispatch({
+                    type: LOGIN,
+                    payload: {
+                        user: user,
+                        token: token
+                    }
+                });
+            } else if (dispatch) {
+                Auth.logout();
+                dispatch({
+                    type: LOGOUT
+                });
+            } 
+        }
+    }, [loading]);
+
+    return (
+        // <div>
+        //     {props.children}
+        // </div>
+        <></>
+    )
+}
